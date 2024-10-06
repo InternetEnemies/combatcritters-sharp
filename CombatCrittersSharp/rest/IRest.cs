3299628eead9830d5b3cc750f4aace046d69dad1
VanJackal <v@njackal.com>
2024-10-06 12:36:21 -0500

    docs

--- CombatCrittersSharp/rest/IRest.cs
+++ CombatCrittersSharp/rest/IRest.cs
@@ -16,4 +16,25 @@ public interface IRest
     /// <param name="endpoint">endpoint to GET</param>
     /// <returns>response from the endpoint</returns>
     Task<HttpResponseMessage> Get(string endpoint);
+    
+    /// <summary>
+    /// PATCH the endpoint
+    /// </summary>
+    /// <param name="endpoint"></param>
+    /// <param name="body"></param>
+    /// <returns></returns>
+    Task<HttpResponseMessage> Patch(string endpoint, object body);
+    /// <summary>
+    /// PUT the endpoint
+    /// </summary>
+    /// <param name="endpoint"></param>
+    /// <param name="body"></param>
+    /// <returns></returns>
+    Task<HttpResponseMessage> Put(string endpoint, object body);
+    /// <summary>
+    /// DELETE the endpoint
+    /// </summary>
+    /// <param name="endpoint"></param>
+    /// <returns></returns>
+    Task<HttpResponseMessage> Delete(string endpoint);
 }
