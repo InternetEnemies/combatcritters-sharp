99bb1e4e8e6e6ff0e01ec830996392ef87337de6
jackal <v@njackal.com>
2024-10-09 07:44:03 -0500

    defined interfaces

--- CombatCrittersSharp/managers/interfaces/IDeckManager.cs
+++ CombatCrittersSharp/managers/interfaces/IDeckManager.cs
@@ -1,3 +1,5 @@
+using CombatCrittersSharp.objects.deck;
+
 namespace CombatCrittersSharp.managers.interfaces;
 
 /// <summary>
@@ -5,5 +7,19 @@ namespace CombatCrittersSharp.managers.interfaces;
 /// </summary>
 public interface IDeckManager
 {
-    
+    /// <summary>
+    /// get the users decks
+    /// </summary>
+    Task<List<IDeck>> GetDecks();
+    /// <summary>
+    /// create a new deck with the given name
+    /// </summary>
+    /// <param name="name">name for the new deck</param>
+    /// <returns>the newly created deck</returns>
+    Task<IDeck> CreateDeck(string name);
+    /// <summary>
+    /// delete a deck
+    /// </summary>
+    /// <param name="deck">deck to delete</param>
+    Task DeleteDeck(IDeck deck);
 }
