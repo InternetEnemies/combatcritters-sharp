1f68782fae25f5f74f6760a7e848faad807ecf4b
jackal <v@njackal.com>
2024-10-09 07:55:17 -0500

    adjusted decks init

--- CombatCrittersSharp/managers/DeckManager.cs
+++ CombatCrittersSharp/managers/DeckManager.cs
@@ -1,11 +1,28 @@
 using CombatCrittersSharp.managers.interfaces;
+using CombatCrittersSharp.objects.deck;
+using CombatCrittersSharp.objects.user;
 
 namespace CombatCrittersSharp.managers;
 
 /// <summary>
 /// manage a users decks
 /// </summary>
-public class DeckManager: IDeckManager
+public class DeckManager(IClient client, IUser user): IDeckManager
 {
-    
+    private readonly IClient _client = client;
+    private readonly IUser _user = user;
+    public Task<List<IDeck>> GetDecks()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IDeck> CreateDeck(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteDeck(IDeck deck)
+    {
+        throw new NotImplementedException();
+    }
 }
