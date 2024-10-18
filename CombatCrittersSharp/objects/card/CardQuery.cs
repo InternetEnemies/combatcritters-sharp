df65bdfdb82378b6f15d0cd9f06162c47285c534
somto idafum <idafum@example.com>
2024-10-18 00:37:11 -0500

    fix CardQuery

--- CombatCrittersSharp/objects/card/CardQuery.cs
+++ CombatCrittersSharp/objects/card/CardQuery.cs
@@ -1,18 +1,68 @@
 using CombatCrittersSharp.objects.card.Interfaces;
+using static CombatCrittersSharp.objects.card.Interfaces.ICardQuery;
 
 namespace CombatCrittersSharp.objects.card
 {
     public class CardQuery : ICardQuery
     {
-        private readonly string _queryString;
-        public CardQuery(string queryString)
+        private readonly int _cost;
+        private readonly bool _costLess;
+        private readonly List<int> _ids;
+        private readonly CardOrder _order;
+        private readonly bool  _owned;
+        private readonly List<int> _rarities;
+        private readonly bool _raritiesInclude;
+
+        public CardQuery(int cost, bool costLess, List<int> ids, CardOrder order, bool owned, List<int> rarities, bool rarityInclude)
         {
-            _queryString = queryString;
+            _cost = cost;
+            _costLess = costLess;
+            _ids = ids;
+            _order = order;
+            _owned = owned;
+            _rarities = rarities;
+            _raritiesInclude = rarityInclude;
         }
 
+        /// <summary>
+        /// This maps each CardOder enum value to its corresponding string.
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        private static string MapOrderToString(CardOrder order)
+        {
+            return order switch
+            {
+                CardOrder.NONE => "",
+                CardOrder.ID => "ID",
+                CardOrder.NAME => "NAME",
+                CardOrder.PLAYCOST => "PLAY_COST",
+                CardOrder.RARITY => "RARITY",
+                _ => throw new ArgumentOutOfRangeException(nameof(order), order, null)
+        
+            };
+        }
+        
         public string GetQueryString()
         {
-            return _queryString;
+
+            //Intialize an List to hold query strings
+            List<string> queryStrings = [];
+
+            if (_cost > 0) queryStrings.Add($"cost={_cost}");
+            if (_costLess) queryStrings.Add($"costLess={_costLess}");
+            if (_ids.Count != 0) queryStrings.Add($"ids={string.Join(",", _ids)}");
+
+            string orderString = MapOrderToString(_order);
+            if (!string.IsNullOrEmpty(orderString)) queryStrings.Add($"order={orderString}");
+
+
+            if (_owned) queryStrings.Add($"owned={_owned}");
+            if (_rarities.Count != 0) queryStrings.Add($"rarities={string.Join(",", _rarities)}");
+            if (_raritiesInclude) queryStrings.Add($"rarityInclude={_raritiesInclude}");
+
+            return string.Join("&", queryStrings);
         }
     }
 }
--- CombatCrittersSharp/objects/card/Interfaces/ICardQuery.cs
+++ CombatCrittersSharp/objects/card/Interfaces/ICardQuery.cs
@@ -1,8 +1,21 @@
-//This interface represents the final query structure build by ICardQueryBuilder
 namespace CombatCrittersSharp.objects.card.Interfaces
 {
     public interface ICardQuery
     {
+        /// <summary>
+        /// Get the query string used for the request
+        /// </summary>
+        /// <returns></returns>
         string GetQueryString(); // Return the query string
+
+        public enum CardOrder
+        {
+            NONE, // will map to ""
+            ID, // will map to "ID"
+            NAME, // will map to "NAME"
+            PLAYCOST, // will map to "PLAY_COST
+            RARITY // will map to "RARITY"
+        }
+
     }
 }
