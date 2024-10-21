e0f3c9b37604bad872c684e432b2f55a7756404c
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-10-20 21:37:18 -0500

    created pack routes

---
+++ CombatCrittersSharp/rest/routes/PackRoutes.cs
@@ -0,0 +1,45 @@
+namespace CombatCrittersSharp.rest.routes;
+
+public static class PackRoutes
+{
+    /// <summary>
+    /// - GET
+    /// - POST
+    /// </summary>\
+    /// <returns> route for packs</returns>
+    /// <returns> route for creating packs</returns>
+    public static string Packs()
+    {
+        return "/packs";
+    }
+
+    /// <summary>
+    /// - GET
+    /// </summary>
+    /// <param name="packId"></param>
+    /// <returns> route for a pack</returns>
+    public static string Pack(int packId)
+    {
+        return $"/packs/{packId}";
+    }
+
+    /// <summary>
+    /// - GET
+    /// </summary>
+    /// <param name="packId"></param>
+    /// <returns> route for the contents of a pack</returns>
+    public static string PackCards(int packId)
+    {
+        return $"/packs/{packId}/contents";
+    }
+
+    /// <summary>
+    /// - GET
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <returns> route for a users packs</returns>
+    public static string UserPacks(int userId)
+    {
+        return $"/users/{userId}/packs";
+    }
+}
