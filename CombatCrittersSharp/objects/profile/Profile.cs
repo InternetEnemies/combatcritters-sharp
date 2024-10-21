9d9bdfc869c16dcae3fb1b6300e7f8e0f7350cf6
somto idafum <idafum@example.com>
2024-10-21 18:28:14 -0500

    write setDeck() method

--- CombatCrittersSharp/objects/profile/Profile.cs
+++ CombatCrittersSharp/objects/profile/Profile.cs
@@ -11,14 +11,20 @@ public class Profile : IProfile
 {
     private readonly IClient _client;
     private readonly IUser _user;
-    public Task<IDeck>? featured_deck { get; }
+
 
     public Profile(IClient client, IUser user)
     {
         this._client = client;
         this._user = user;
-        featured_deck = null;
     }
+
+    /// <summary>
+    /// Get the profile of the user, which is the featured deck.
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
+    /// <exception cref="AuthException"></exception>
     public async Task<IDeck?> GetDeck()
     {
         try
@@ -38,6 +44,37 @@ public class Profile : IProfile
         catch (RestException e)
         {
             throw new AuthException("Error getting featured deck", e);
+        }
+    }
+
+    /// <summary>
+    /// Set the profile of the user, which is the featured deck.
+    /// </summary>
+    /// <param name="deck"></param>
+    /// <returns></returns>
+    /// <exception cref="AuthException"></exception>
+    public async Task SetDeck(IDeck deck)
+    {
+        try
+        {
+            // Create the deck details payload from the passed deck
+            var deckPayload = new DeckDetailsPayload(
+                deck.DeckId,
+                deck.Name
+            );
+
+            //Create the profile payload with the deck details
+            var profilePayload = new ProfilePayload(
+                featured_deck: deckPayload
+            );
+
+            //Send a PUT request to the backend to update the user's featured deck
+            await _client.Rest.Put(ProfileRoutes.Profile(_user.Id), profilePayload);
+
+        }
+        catch (RestException e)
+        {
+            throw new AuthException("Error setting featured deck", e);
         }
     }
 }
