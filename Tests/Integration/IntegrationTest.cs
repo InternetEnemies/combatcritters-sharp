01261e72654b45a42679c5380a0efdccc2c72c27
jackal <v@njackal.com>
2024-10-21 11:56:04 -0500

    working containers

--- Tests/Integration/IntegrationTest.cs
+++ Tests/Integration/IntegrationTest.cs
@@ -1,39 +1,54 @@
 using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Containers;
+using DotNet.Testcontainers.Networks;
 using Testcontainers.PostgreSql;
 
 namespace Tests.Integration;
 
 public class IntegrationTest
 {
+    private static readonly string NetworkName = "critter_net";
+    
     public static readonly string ApiUrl = "http://localhost:4000";
     private static readonly string ApiImage = "vanjackal/combatcritters:latest";
-
+    
     private static readonly string DbName = "critter_db";
     private static readonly string DbUser = "critter_db";
     private static readonly string DbPass = "critter_db";
-    private static readonly string DbHost = "localhost:5432";
+    private static readonly string DbHost = "postgres";
+    private static readonly int DbPort = 5432;
+    
 
     private IContainer _apiContainer;
     private PostgreSqlContainer _postgresContainer;
+    private INetwork _network;
     
     [SetUp]
     public virtual async Task Setup()
     { //! this isn't really a great way of doing this but TestContainers for .NET doesn't support compose yet (java does ;-;)
+        _network = new NetworkBuilder()
+            .WithName(NetworkName)
+            .Build();
+        await _network.CreateAsync();
+        
         _postgresContainer = new PostgreSqlBuilder()
             .WithDatabase(DbName)
             .WithUsername(DbUser)
             .WithPassword(DbPass)
-            .WithPortBinding(5432)
+            .WithPortBinding(DbPort)
+            .WithNetwork(_network)
+            .WithNetworkAliases(DbHost)
             .Build();
         await _postgresContainer.StartAsync();
+        
         _apiContainer = new ContainerBuilder()
             .WithImage(ApiImage)
             .WithPortBinding(4000, 8080)
             .DependsOn(_postgresContainer)
-            .WithEnvironment("DB_HOST",_postgresContainer.Hostname)
+            .WithEnvironment("DB_HOST",$"{DbHost}:{DbPort}")
             .WithEnvironment("DB_USER",DbUser)
             .WithEnvironment("DB_PASS",DbPass)
+            .WithNetwork(_network)
             .Build();
         await _apiContainer.StartAsync();
     }
@@ -43,5 +58,6 @@ public class IntegrationTest
     {
         await _apiContainer.DisposeAsync();
         await _postgresContainer.DisposeAsync();
+        await _network.DisposeAsync();
     }
 }
